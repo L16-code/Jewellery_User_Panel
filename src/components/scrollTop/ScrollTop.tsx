91f325dc5694a777bb43fe645714649bc0d75c9a
L16-code <lavishjain2002@gmail.com>
2024-08-02 17:26:09 +0530

    project initialized

---
+++ src/components/scrollTop/ScrollTop.tsx
@@ -0,0 +1,24 @@
+
+const ScrollTop = () => {
+    return (
+        <button id="scroll__top">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ionicon"
+                viewBox="0 0 512 512"
+            >
+                <path
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={48}
+                    d="M112 244l144-144 144 144M256 120v292"
+                />
+            </svg>
+        </button>
+
+    )
+}
+
+export default ScrollTop
