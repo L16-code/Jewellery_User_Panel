418eac3bc0881160ee92651307824544bcbefcea
L16-code <lavishjain2002@gmail.com>
2024-08-13 07:00:38 +0530

    added contact and my account pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Contact from "./pages/Contact/Contact";
+import MyAccount from "./pages/MyAccount/MyAccount";
 import Shops from "./pages/shops/shops";
 import SingleProductView from "./pages/single product/SingleProductView";
 
 const App: React.FC = () => {
   return (
-      <Shops/>
+      <Contact/>
   );
 }
 
--- src/components/scrollTop/ScrollTop.tsx
+++ src/components/scrollTop/ScrollTop.tsx
@@ -1,5 +1,18 @@
 
 const ScrollTop = () => {
+    const scrollTop = document.getElementById("scroll__top");
+    if (scrollTop) {
+        scrollTop.addEventListener("click", function () {
+            window.scroll({ top: 0, left: 0, behavior: "smooth" });
+        });
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 300) {
+                scrollTop.classList.add("active");
+            } else {
+                scrollTop.classList.remove("active");
+            }
+        });
+    }
     return (
         <button id="scroll__top">
             <svg
